2421bafa0ceca8a064ca0fc680517685eb8184d0
Leo Preciado <leoipn@gmail.com>
2021-07-17 03:25:39 -0500

    Leaflet - Final

--- Leaflet-Step-2/static/js/logic.js
+++ Leaflet-Step-2/static/js/logic.js
@@ -1,61 +1,112 @@
-function createMap(bikeStations) {
+var markeColors = ["#a3f600","#dcf400","#f7db11","#fdb72a","#fca35d","#ff5f65"];
 
-    // Create the tile layer that will be the background of our map
-    var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-      maxZoom: 18,
-      id: "light-v10",
-      accessToken: API_KEY
-    });
-  
-    // Create a baseMaps object to hold the lightmap layer
-    var baseMaps = {
-      "Light Map": lightmap
-    };
-  
-    // Create an overlayMaps object to hold the bikeStations layer
-    var overlayMaps = {
-      "Bike Stations": bikeStations
-    };
-  
-    // Create the map object with options
-    var map = L.map("mapid", {
-      center: [40.73, -74.0059],
-      zoom: 12,
-      layers: [lightmap, bikeStations]
-    });
-  
-    // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
-    L.control.layers(baseMaps, overlayMaps, {
-      collapsed: false
-    }).addTo(map);
+// Create the tile layer that will be the background of our map
+var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "light-v10",
+  accessToken: API_KEY
+});
+
+var satellitemap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/satellite-v9",
+  accessToken: API_KEY
+});
+
+var tectonic = new L.layerGroup();
+
+// Create the map object with options
+var map = L.map("mapid", {
+  center: [35, -110],
+  zoom: 3,
+  layers:[satellitemap]
+});
+
+function markerColor(depth) {
+  var index = Math.round(depth/6);
+  return markeColors[index];
+}
+    
+d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(data=>{
+  L.geoJson(data,{
+    color: "#FFA500",
+    weight: 3
+  }).addTo(tectonic)
+
+  tectonic.addTo(map);
+});
+
+// Perform an API call to the USGS API to get earthquake information. Call createMarkers when complete
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson").then(response =>{
+  // Pull the "features" property off of response.features
+  var features = response.features;
+
+  // Initialize an array to hold bike markers
+  var earthQuakeMarkers = [];
+
+  // Loop through the earthquakes array
+  for (var index = 0; index < features.length; index++) {
+    var feature = features[index];
+    var coordinates = feature.geometry.coordinates;
+    var property = feature.properties;
+
+    var earthquakeMarker = L.circleMarker([coordinates[1], coordinates[0]], {
+        fillOpacity: .85,
+        weight:.5,
+        color: "black",
+        fillColor: markerColor(coordinates[2]),
+        radius: (property.mag * 5)
+      }).bindPopup("<h1>" + property.title + "</h1> <hr> <h3>Time: " + new Date(property.time) + "</h3> <hr> <h3>Depth: " + coordinates[2] + "</h3>");
+    
+    // Add the marker to the earthQuakeMarkers array
+    earthQuakeMarkers.push(earthquakeMarker);
   }
+
+  // Create a layer group made from the earthQuakeMarkers markers array, pass it into the createMap function
+  var earthQuakeLayer  = L.layerGroup(earthQuakeMarkers);
+
+  // Create a baseMaps object to hold the lightmap layer
+  var baseMaps = {
+    "Light Map": lightmap,
+    "Satellite View": satellitemap
+  };
+
+  // Create an overlayMaps object to hold the bikeStations layer
+  var overlayMaps = {
+    "Earthquakes": earthQuakeLayer,
+    "Tectonic Plates": tectonic
+  };
   
-  function createMarkers(response) {
-  
-    // Pull the "stations" property off of response.data
-    var stations = response.data.stations;
-  
-    // Initialize an array to hold bike markers
-    var bikeMarkers = [];
-  
-    // Loop through the stations array
-    for (var index = 0; index < stations.length; index++) {
-      var station = stations[index];
-  
-      // For each station, create a marker and bind a popup with the station's name
-      var bikeMarker = L.marker([station.lat, station.lon])
-        .bindPopup("<h3>" + station.name + "<h3><h3>Capacity: " + station.capacity + "</h3>");
-  
-      // Add the marker to the bikeMarkers array
-      bikeMarkers.push(bikeMarker);
-    }
-  
-    // Create a layer group made from the bike markers array, pass it into the createMap function
-    createMap(L.layerGroup(bikeMarkers));
-  }
-  
-  
-  // Perform an API call to the Citi Bike API to get station information. Call createMarkers when complete
-  d3.json("https://gbfs.citibikenyc.com/gbfs/en/station_information.json").then(createMarkers);
-  
+  // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(map);
+
+  // Create a legend to display information about our map
+  var info = L.control({
+    position: "bottomright"
+  });
+
+  // When the layer control is added, insert a div with the class of "legend"
+  info.onAdd = function() {
+    var div = L.DomUtil.create("div", "legend");
+    var limits = ["-10-10","10-30","30-50","50-70","70-90","90+"];
+    var colors = markeColors;
+    var labels = [];
+
+    limits.forEach(function(limit, index) {
+      labels.push("<li style='background-color:"+ colors[index] +"'></li><div class='max'>"+ limits[index] +" </div>");
+    });
+
+    div.innerHTML +='<ul>' + labels.join('') + '</ul>';
+
+    return div;
+  };
+  // Add the info legend to the map
+  info.addTo(map);
+  earthQuakeLayer.addTo(map);
+});
